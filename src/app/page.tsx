3c17051325d6103db3e322dc946dd2bf838057cc
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-08 16:11:07 -0300

    app-pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Presentation from "@/components/Home/Presentation";
+import TrandingProducts from "@/components/TrandingProducts";
 
 export default function Home() {
   return (
     <>
       <Presentation />
 
-      
+      <TrandingProducts />
     </>
   )
 }
