4f09a8bbccd8f515f6bc95575d72ca7ac0985624
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-08 14:19:51 -0300

    components

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='w-full h-[72px] flex items-center justify-between p-nav'>
+
+    </nav>
+  )
+}
+
+export default NavBar
