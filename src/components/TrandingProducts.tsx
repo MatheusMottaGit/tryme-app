57765649e092bed2d33a375ee4f650dcfd4327d4
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-08 16:11:22 -0300

    components

---
+++ src/components/TrandingProducts.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TrandingProducts = () => {
+  return (
+    <section className='bg-white h-screen w-screen p-nav flex items-center justify-center'>
+      <div className='w-full'>
+        
+      </div>
+    </section>
+  )
+}
+
+export default TrandingProducts
