f3120e5ded57f3b335331b8bfd6c5b221e05f2bf
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-09 13:15:33 -0300

    app-pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Presentation from "@/components/Home/Presentation";
 import OffersAndPromotions from "@/components/Offers/OffersAndPromotions";
 import TrandingProducts from "@/components/Tranding/TrandingProducts";
 
-
 export default function Home() {
   return (
     <>
---
+++ src/app/product/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='w-screen h-screen bg-white'>
+      dqjdwj
+    </div>
+  )
+}
+
+export default Page
